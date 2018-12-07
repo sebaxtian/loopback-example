5bdb4bd5f3fbf2f71b4ea9c32b3718519009ae38
Sebastian Rios Sabogal <sebaxtianrioss@gmail.com>
2018-12-07 05:04:49 +0000

    Agrego Validador Asincrono

--- server/models/report.js
+++ server/models/report.js
@@ -1,5 +1,19 @@
 'use strict';
 
-module.exports = function(Report) {
+module.exports = function (Report) {
+    function validatorAsyncForGuardId(error, next) {
+        const Guard = Report.app.models.Guard;
 
+        Guard.exists(this.guardId,
+            function (errorExists, instanceExists) {
+                if (errorExists || !instanceExists) {
+                    error();
+                }
+                next();
+            });
+    }
+
+    Report.validateAsync('guardId', validatorAsyncForGuardId, {
+        message: 'Guard doesn\'t exist',
+    });
 };
